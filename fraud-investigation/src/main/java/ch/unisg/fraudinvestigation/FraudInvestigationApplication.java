3f1bffc8455dbd96189359636ae637584c7d3067
Marcel <marcel@maece.ch>
2022-04-03 14:34:13 +0200

    init services

---
+++ fraud-investigation/src/main/java/ch/unisg/fraudinvestigation/FraudInvestigationApplication.java
@@ -0,0 +1,13 @@
+package ch.unisg.fraudinvestigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudInvestigationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudInvestigationApplication.class, args);
+	}
+
+}
---
+++ transaction-dispute/src/test/java/ch/unisg/transactiondispute/TransactionDisputeApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.unisg.transactiondispute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionDisputeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
