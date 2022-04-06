25b5aeb75c997f7e97a8741d29bdf75f43ac791d
jo9x <jonas.hermann@web.de>
2022-04-06 13:22:50 +0200

    Added new pin service

--- pin/src/main/java/ch/unisg/pin/PinApplication.java
+++ pin/src/main/java/ch/unisg/pin/PinApplication.java
@@ -1,23 +1,13 @@
 package ch.unisg.pin;
 
-import ch.unisg.pin.dto.CamundaMessageDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.function.Consumer;
 
 @SpringBootApplication
 public class PinApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PinApplication.class, args);
-	}
-
-	@Bean
-	public Consumer<CamundaMessageDto> pin(){
-		System.out.println("Got something");
-		return null;
 	}
 
 }
