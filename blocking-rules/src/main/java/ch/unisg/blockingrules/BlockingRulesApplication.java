7459e7effc4d070af03dae191d617ee130f7a9ac
jo9x <jonas.hermann@web.de>
2022-04-06 16:42:19 +0200

    Added Blocking Rules service

---
+++ blocking-rules/src/main/java/ch/unisg/blockingrules/BlockingRulesApplication.java
@@ -0,0 +1,13 @@
+package ch.unisg.blockingrules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockingRulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockingRulesApplication.class, args);
+	}
+
+}
