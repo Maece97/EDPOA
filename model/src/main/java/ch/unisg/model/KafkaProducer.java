c521959e15cdbd76776b83e7449f8a88df9dc577
Marcel <marcel@maece.ch>
2022-04-19 08:18:45 +0700

    naming

--- model/src/main/java/ch/unisg/model/KafkaProducer.java
+++ model/src/main/java/ch/unisg/model/KafkaProducer.java
@@ -6,7 +6,6 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.function.StreamBridge;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import ch.unisg.model.domain.Transaction;
@@ -19,13 +18,13 @@ public class KafkaProducer {
     @Autowired
     private StreamBridge streamBridge;
 
-    @Scheduled(cron = "*/10 * * * * *")
-    public void transaction(){
-        for  (int i = 0; i < 1; i++) {
-            TransactionTransferObject tto = new TransactionTransferObject(UUID.randomUUID(),new Timestamp(System.currentTimeMillis()),"Adidas",12.45,"EUR","123",456);
-            streamBridge.send("transaction-out-0", "kafka1",new Message<TransactionTransferObject>("transaction",tto));
-        }
-    }
+    // @Scheduled(cron = "*/10 * * * * *")
+    // public void transaction(){
+    //     for  (int i = 0; i < 1; i++) {
+    //         TransactionTransferObject tto = new TransactionTransferObject(UUID.randomUUID(),new Timestamp(System.currentTimeMillis()),"Adidas",12.45,"EUR","123",456);
+    //         streamBridge.send("transaction-out-0", "kafka1",new Message<TransactionTransferObject>("transaction",tto));
+    //     }
+    // }
 
     public void possibleFraudDetected(Transaction t) {
         streamBridge.send("possibleFraudTransaction-out-0", "kafka1",new Message<TransactionTransferObject>("transaction",new TransactionTransferObject(t)));
