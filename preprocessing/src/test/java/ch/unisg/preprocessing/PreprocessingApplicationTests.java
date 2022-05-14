29c274258c180cb9a8a6d650cdd53befa7027751
jo9x <jonas.hermann@web.de>
2022-05-14 10:30:35 +0200

    initiated preprocessing service

---
+++ preprocessing/src/test/java/ch/unisg/preprocessing/PreprocessingApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.unisg.preprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreprocessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
